dc806d182eb6153c535c2d38f4a06dbdb335927d
marlisonmourao <bentesmourao@gmail.com>
2023-03-09 01:09:27 -0400

    creating search component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 
 import { PriceHighlight, TransactionsTable, TrasactionsContainer } from './styles'
 
@@ -10,6 +11,8 @@ export function Transactions() {
       <Summary />
 
       <TrasactionsContainer>
+        <SearchForm />
+
         <TransactionsTable>
           <tbody>
             <tr>
