f2e63ec598f09da52236879f28e5d574e1be9a76
marlisonmourao <bentesmourao@gmail.com>
2023-03-10 01:05:28 -0400

    turning income and outcome calculation into a hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,29 +1,13 @@
-import { SummaryCard, SummaryContainer } from "./styles";
+import { SummaryCard, SummaryContainer } from "./styles"
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 
-import { useTransactions } from "../../hooks/useTransactions";
-import { priceFormatter } from "../../utils/formatter";
+import { priceFormatter } from "../../utils/formatter"
+
+import { useSummary } from "../../hooks/useSummary"
 
 export function Summary() {
-  const { transactions } = useTransactions()
   
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if(transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    }, 
-    {
-      income: 0, 
-      outcome: 0,
-      total: 0
-    })
+  const summary = useSummary()
 
   return (
     <SummaryContainer>
@@ -54,5 +38,5 @@ export function Summary() {
         <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
-  );
+  )
 }
