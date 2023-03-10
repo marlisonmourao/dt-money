ad49beecc391b7d632f140be1ff6e31241bbed27
marlisonmourao <bentesmourao@gmail.com>
2023-03-10 00:59:34 -0400

    formatting values ​​and dates

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 import { useTransactions } from '../../hooks/useTransactions';
+import { dateFormatter, priceFormatter } from '../../utils/formatter';
 import { SearchForm } from './components/SearchForm';
 
 import { PriceHighlight, TransactionsTable, TrasactionsContainer } from './styles'
@@ -22,10 +23,13 @@ export function Transactions() {
               <tr key={transaction.id}>
                 <td width="50%">{transaction.description}</td>
                 <td>
-                  <PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.type === "outcome" && "- "}
+                    {priceFormatter.format(transaction.price)}
+                  </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
             ))}
           </tbody>
